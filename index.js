0fbf74371a1c04b10bb60cdea0da330932032f39
Cody Jones <magerx1794@gmail.com>
2025-09-21 21:12:20 -0500

    CJ | fixing imports

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-const { generateNewEnvList } = require('./lib/generateNewEnvList.js');
+// Main entry point for @mono-labs/cli package
+export { boot } from './lib/commands/build-process/boot.js';
+export { buildCommands } from './lib/commands/build-process/cliFactory.js';
+export { runHasteCommand } from './lib/commands/build-process/runHasteCommand.js';
+export { verifyOptionValue } from './lib/commands/build-process/validators.js';
+export {
+	setData,
+	getData,
+	mergeData,
+	replaceTokens,
+} from './lib/commands/build-process/dataLayer.js';
+export {
+	getHasteConfig,
+	getRootDirectory,
+	getRootJson,
+} from './lib/commands/loadFromRoot.js';
+import { generateNewEnvList } from './lib/commands/build-process/env.js';
+export { program } from './lib/app.js';
 
-module.exports = { generateNewEnvList };
+// Default export for convenience
+export default {
+	generateNewEnvList,
+	boot,
+	buildCommands,
+	runHasteCommand,
+	verifyOptionValue,
+	setData,
+	getData,
+	mergeData,
+	replaceTokens,
+	getHasteConfig,
+	getRootDirectory,
+	getRootJson,
+	program,
+};
