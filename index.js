012ca063f89226e4b0a3ea77a10b85f690e52c91
Cody Jones <magerx1794@gmail.com>
2025-10-10 14:57:53 -0500

    0.0.135

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Main entry point for @mono-labs/cli package
 import { generateNewEnvList } from './lib/generateNewEnvList.js';
-import { filterUnwantedEnvVars } from './lib/filterUnwantedEnvVars.js';
+import {
+	filterUnwantedEnvVars,
+	filterUnwantedEnvVarsEAS,
+} from './lib/filterUnwantedEnvVars.js';
 // Default export for convenience
 export default {
 	generateNewEnvList,
 	filterUnwantedEnvVars,
+	filterUnwantedEnvVarsEAS,
 };
