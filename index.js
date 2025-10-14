ff94edcca2cc3129df13a621c2bbe61065525e64
Cody Jones <magerx1794@gmail.com>
2025-10-14 10:14:43 -0500

    CJ | Adding fixes

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import {
 	filterUnwantedEnvVars,
 	filterUnwantedEnvVarsEAS,
 } from './lib/filterUnwantedEnvVars.js';
+import { replaceTokens } from './lib/commands/build-process/dataLayer.js';
 // Default export for convenience
 export default {
+	replaceTokens,
 	generateNewEnvList,
 	filterUnwantedEnvVars,
 	filterUnwantedEnvVarsEAS,
